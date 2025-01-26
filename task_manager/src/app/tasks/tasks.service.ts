d02044923939b9001fedc9115c00b53de3dd7e30
William Jackson <williampj@gmail.com>
2025-01-26 09:30:03 +0100

    Add task manager app

---
+++ task_manager/src/app/tasks/tasks.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from "@angular/core";
+import { DUMMY_TASKS } from "../../assets/dummy-tasks"
+import { NewTaskData } from "./task/task.model";
+
+@Injectable({ providedIn: 'root' })
+export class TasksService {
+  private tasks = DUMMY_TASKS;
+
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
+  getUserTasks(userId: string) {
+    return this.tasks.filter((task) => task.userId === userId);
+  }
+
+  addTask(taskData: NewTaskData, userId: string) {
+    this.tasks.unshift({
+        id: new Date().getTime().toString(), 
+        userId,
+        title: taskData.title,
+        summary: taskData.summary,
+        dueDate: taskData.date,
+    });
+    this.saveTasks();
+  }
+
+  removeTask(id: string){
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
+}
---
+++ task_manager/src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component, computed, Input, input, Output, EventEmitter, output } from '@angular/core';
+import { User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [CardComponent], 
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+
+export class UserComponent {
+  @Input({required: true}) user!: User;
+  @Input({required: true}) selected!: boolean;
+
+  @Output() select = new EventEmitter<string>();
+  
+  get imagePath() { 
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
